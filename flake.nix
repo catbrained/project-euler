c70568bec04d2679a5d021ad4af5d32923142733
Linda Siemons <linda@catbrained.dev>
2025-01-26 16:59:36 +0100

    feat: add cargo-expand to dev shell
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- flake.nix
+++ flake.nix
@@ -147,7 +147,7 @@
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
-            # pkgs.ripgrep
+            pkgs.cargo-expand
           ];
         };
       });
