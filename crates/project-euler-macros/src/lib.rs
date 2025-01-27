506ccbc709cb12207f21d579aeb60625add53b08
Linda Siemons <linda@catbrained.dev>
2025-01-27 15:23:00 +0100

    feat(macros): generate boilerplate for calling solutions
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- crates/project-euler-macros/src/lib.rs
+++ crates/project-euler-macros/src/lib.rs
@@ -6,13 +6,60 @@ use syn::{parse_macro_input, LitInt};
 pub fn gen_mods(input: TokenStream) -> TokenStream {
     let lit = parse_macro_input!(input as LitInt);
     let value = lit.base10_parse::<u16>().unwrap();
-    let idents = (1..=value).map(|n| {
-        let mod_name = format!("p{:04}", n);
-        syn::Ident::new(&mod_name, lit.span())
-    });
+    let idents: Vec<_> = (1..=value)
+        .map(|n| {
+            let mod_name = format!("p{:04}", n);
+            (n, syn::Ident::new(&mod_name, lit.span()))
+        })
+        .collect();
+
+    let mut modules = Vec::new();
+    for (_, ident) in idents.iter() {
+        let module = quote! {
+            mod #ident;
+        };
+        modules.push(module);
+    }
+
+    let mut match_arms = Vec::new();
+    for (n, ident) in idents.iter() {
+        let arm = quote! {
+            #n => #ident::solve(),
+        };
+        match_arms.push(arm);
+    }
+
+    let mut solutions = Vec::new();
+    for (_, ident) in idents.iter() {
+        let solution = quote! {
+            solutions.push(#ident::solve());
+        };
+        solutions.push(solution);
+    }
+
+    let functions = quote! {
+        /// Solve problem number `n`.
+        pub fn solve(n: u16) -> impl ::std::fmt::Display {
+            match n {
+                #(#match_arms)*
+                _ => unimplemented!(),
+            }
+        }
+
+        /// Solve all problems.
+        pub fn solve_all() -> impl ::std::iter::Iterator<Item = impl ::std::fmt::Display> {
+            let mut solutions = ::std::vec::Vec::new();
+            #(#solutions)*
+
+            solutions.into_iter()
+        }
+    };
 
     quote! {
-        #( pub mod #idents; )*
+        #(#modules)*
+        /// The highest numbered problem that has been solved.
+        pub const PMAX: u16 = #value;
+        #functions
     }
     .into()
 }
--- crates/project-euler/src/main.rs
+++ crates/project-euler/src/main.rs
@@ -1,6 +1,30 @@
-use project_euler::problems;
+use project_euler::problems::{solve, solve_all, PMAX};
 
 fn main() {
-    let result = problems::p0002::solve();
-    println!("{result}");
+    let solve_all = || {
+        let results = solve_all();
+        for (n, result) in results.enumerate() {
+            println!("Result for problem {n}:\n\t{result}");
+        }
+    };
+
+    if let Some(command) = std::env::args().nth(1) {
+        match command.as_str() {
+            "--all" => solve_all(),
+            n if n.parse::<u16>().is_ok() => {
+                let n = n.parse::<u16>().expect("Should be valid u16");
+                if !(1..=PMAX).contains(&n) {
+                    eprintln!("Problem must be between 1 and {PMAX}");
+                } else {
+                    let result = solve(n);
+                    println!("Result for problem {n}:\n\t{result}");
+                }
+            }
+            o => {
+                eprintln!("Unrecognized option `{o}`. Use `--all` to run all problems, or a number between 1 and {PMAX}");
+            }
+        }
+    } else {
+        solve_all()
+    }
 }
