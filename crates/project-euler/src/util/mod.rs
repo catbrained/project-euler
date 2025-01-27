d83c4a26c6a60893ef3d45e1edb7221c7fe29de2
Linda Siemons <linda@catbrained.dev>
2025-01-27 16:24:47 +0100

    tests: check that fibonacci matches first 40 numbers
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- crates/project-euler/src/util/mod.rs
+++ crates/project-euler/src/util/mod.rs
@@ -24,3 +24,22 @@ pub(crate) fn fibonacci(limit: u64) -> impl Iterator<Item = u64> {
         }
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fibonacci_matches_first_40_numbers() {
+        // Numbers from https://oeis.org/A000045
+        let first_40 = vec![
+            1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765,
+            10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269,
+            2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155,
+        ];
+
+        let res: Vec<_> = fibonacci(*first_40.last().unwrap()).collect();
+
+        assert_eq!(first_40, res);
+    }
+}
