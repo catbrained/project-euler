f9b10d4c33f4c84052aa25a136d8c999c7fcd5b4
Linda Siemons <linda@catbrained.dev>
2025-01-28 20:49:34 +0100

    benches: add benchmark for problem 4
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- crates/project-euler/benches/benchmarks.rs
+++ crates/project-euler/benches/benchmarks.rs
@@ -1,9 +1,13 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use project_euler::problems::solve;
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn bench_p0002(c: &mut Criterion) {
     c.bench_function("problem 0002", |b| b.iter_with_large_drop(|| solve(2)));
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn bench_p0004(c: &mut Criterion) {
+    c.bench_function("problem 0004", |b| b.iter_with_large_drop(|| solve(4)));
+}
+
+criterion_group!(benches, bench_p0002, bench_p0004);
 criterion_main!(benches);
