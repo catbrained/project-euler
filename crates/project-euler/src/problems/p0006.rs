ac063a2388f883ea77433befc3d1aa4aaa9a3959
Linda Siemons <linda@catbrained.dev>
2025-01-29 16:17:11 +0100

    feat: solve problem 6
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

---
+++ crates/project-euler/src/problems/p0006.rs
@@ -0,0 +1,20 @@
+use crate::util::sum_of_natural_numbers;
+
+/// The sum of the squares of the first ten natural numbers is,
+/// 1^2 + 2^2 + ... + 10^2 = 385.
+/// The square of the sum of the first ten natural numbers is,
+/// (1 + 2 + ... + 10)^2 = 55^2 = 3025.
+/// Hence the difference between the sum of the squares of the first ten natural numbers
+/// and the square of the sum is 3025 - 385 = 2640.
+/// Find the difference between
+/// the sum of the squares of the first one hundred natural numbers and the square of the sum.
+pub fn solve() -> u64 {
+    let sum_of_squares = sum_of_squares(100);
+    let square_of_sum = sum_of_natural_numbers(100).pow(2);
+
+    square_of_sum - sum_of_squares
+}
+
+fn sum_of_squares(n: u64) -> u64 {
+    (n * (n + 1) * (2 * n + 1)) / 6
+}
