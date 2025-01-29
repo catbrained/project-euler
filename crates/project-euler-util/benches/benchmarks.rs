0528644c362015dcf98b76578cf1167c9fef5aea
Linda Siemons <linda@catbrained.dev>
2025-01-29 22:48:15 +0100

    benches: measure primes
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- crates/project-euler-util/benches/benchmarks.rs
+++ crates/project-euler-util/benches/benchmarks.rs
@@ -1,5 +1,5 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use project_euler_util::fibonacci;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use project_euler_util::{fibonacci, Primes};
 
 fn bench_fibonacci(c: &mut Criterion) {
     c.bench_function("fib 10", |b| {
@@ -16,5 +16,15 @@ fn bench_fibonacci(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_fibonacci);
+fn bench_primes(c: &mut Criterion) {
+    let mut group = c.benchmark_group("primes");
+    for n in [1000, 10_000, 100_000, 1_000_000].iter() {
+        group.bench_with_input(BenchmarkId::from_parameter(n), n, |b, &n| {
+            b.iter_with_large_drop(|| Primes::with_capacity(n).take(n).collect::<Vec<_>>())
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_fibonacci, bench_primes);
 criterion_main!(benches);
