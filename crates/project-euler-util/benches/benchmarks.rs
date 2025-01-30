3d109b9532ca8786f9d8a47a9b432115fdf33360
Linda Siemons <linda@catbrained.dev>
2025-01-30 14:15:00 +0100

    benches: preallocate result vector
    This doesn't seem to bring any noticeable improvement for the primes
    benchmark, but the fibonacci one does show some pretty good speedups.
    
    Not sure how much I want to trust those benchmarks though, since they
    were run in a very ad-hoc fashion on my laptop with other stuff running
    in the background.
    
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- crates/project-euler-util/benches/benchmarks.rs
+++ crates/project-euler-util/benches/benchmarks.rs
@@ -5,7 +5,11 @@ fn bench_fibonacci(c: &mut Criterion) {
     let mut group = c.benchmark_group("fibonacci");
     for n in [10, 20, 40, 60].iter() {
         group.bench_with_input(BenchmarkId::from_parameter(n), n, |b, &n| {
-            b.iter_with_large_drop(|| fibonacci().take(n).collect::<Vec<_>>())
+            b.iter_with_large_drop(|| {
+                let mut res = Vec::with_capacity(n);
+                res.extend(fibonacci().take(n));
+                res
+            })
         });
     }
 }
@@ -14,7 +18,11 @@ fn bench_primes(c: &mut Criterion) {
     let mut group = c.benchmark_group("primes");
     for n in [1000, 10_000, 100_000, 1_000_000].iter() {
         group.bench_with_input(BenchmarkId::from_parameter(n), n, |b, &n| {
-            b.iter_with_large_drop(|| Primes::with_capacity(n).take(n).collect::<Vec<_>>())
+            b.iter_with_large_drop(|| {
+                let mut res = Vec::with_capacity(n);
+                res.extend(Primes::with_capacity(n).take(n));
+                res
+            })
         });
     }
     group.finish();
